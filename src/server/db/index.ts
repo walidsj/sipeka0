3c7492f4c687134860e43889d605640228e3ca4f
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-06-04 06:57:42 +0800

    refactor db config env

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,8 +1,9 @@
 import { drizzle } from 'drizzle-orm/mysql2'
 import { createPool, type Pool } from 'mysql2/promise'
-
 import * as schema from './schema'
-import { env } from '@/env'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const globalForDb = globalThis as unknown as {
     conn: Pool | undefined
@@ -11,11 +12,11 @@ const globalForDb = globalThis as unknown as {
 const conn =
     globalForDb.conn ??
     createPool({
-        host: env.DB_HOST,
-        user: env.DB_USER,
-        password: env.DB_PASSWORD,
-        database: env.DB_NAME,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     })
-if (env.NODE_ENV !== 'production') globalForDb.conn = conn
+if (process.env.NODE_ENV !== 'production') globalForDb.conn = conn
 
 export const db = drizzle(conn, { schema, mode: 'default' })
