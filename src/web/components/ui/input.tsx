85da854a8e335e66edb5749a68e06841f00a4a48
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-06-03 22:40:02 +0800

    refactor: fix bug input

--- src/web/components/ui/input.tsx
+++ src/web/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             <input
                 type={type}
                 className={cn(
-                    'flex h-10 w-full rounded-xl border bg-background px-3 py-1 text-sm opacity-75 shadow-sm transition-colors file:border-0 file:bg-background file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:bg-slate-50',
+                    'flex h-10 w-full rounded-xl border bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-background file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:bg-slate-50 disabled:opacity-75',
                     className
                 )}
                 ref={ref}
