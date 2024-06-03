03ae52cc6bfb50c4e0e96edb49d4322093f70a69
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-06-04 04:20:02 +0800

    refactor: add ecosystem config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+    name: 'sipeka0', // Name of your application
+    script: 'bun start', // Entry point of your application
+    interpreter: 'bun', // Path to the Bun interpreter
+}
