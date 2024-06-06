70c68391238d7791e44f0a96353792108e130f47
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-06-06 22:13:43 +0800

    refactor: add new foto

--- src/app/routes/page.tsx
+++ src/app/routes/page.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/web/components/ui/button'
 import { cn } from '@/web/lib/utils'
 import React from 'react'
 import { FaWhatsapp } from 'react-icons/fa'
+import { FaHeart } from 'react-icons/fa6'
 import { FiArrowRight } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
@@ -111,11 +112,8 @@ export default function Home() {
         <div className="flex w-full flex-col">
             <div className="rounded-lg bg-white px-8 pb-28 pt-20">
                 <h2 className="text-5xl">Selamat datang di </h2>
-                <h1 className="text-7xl font-bold">
+                <h1 className="mb-10 text-7xl font-bold">
                     Sistem Informasi Pengelolaan Keuangan
-                </h1>
-                <h1 className="text-7xl font-bold">
-                    Keuangan RSJD Atma Husada Mahakam
                 </h1>
                 <div className="flex gap-2">
                     <Button asChild size="lg">
@@ -132,66 +130,95 @@ export default function Home() {
                     </Button>
                 </div>
             </div>
-            <h2 className="p-8 pt-10 text-center text-6xl font-extrabold">
-                Meet Our Team!
-            </h2>
-            <div className="mx-auto flex gap-3 px-8">
-                <Button
-                    variant="outline"
-                    className={cn(
-                        team === 'Semua' && 'border border-primary text-primary'
-                    )}
-                    onClick={() => setTeam('Semua')}
-                >
-                    Semua
-                </Button>
-                <Button
-                    variant="outline"
-                    className={cn(
-                        team === 'Perbendaharaan' &&
-                            'border border-primary text-primary'
-                    )}
-                    onClick={() => setTeam('Perbendaharaan')}
-                >
-                    Perbendaharaan
-                </Button>
-                <Button
-                    variant="outline"
-                    className={cn(
-                        team === 'Akuntansi' &&
-                            'border border-primary text-primary'
-                    )}
-                    onClick={() => setTeam('Akuntansi')}
-                >
-                    Akuntansi
-                </Button>
-                <Button
-                    variant="outline"
-                    className={cn(
-                        team === 'Verifikasi' &&
-                            'border border-primary text-primary'
-                    )}
-                    onClick={() => setTeam('Verifikasi')}
-                >
-                    JPK Center
-                </Button>
-                <Button
-                    variant="outline"
-                    className={cn(
-                        team === 'Tim Efektif' &&
-                            'border border-primary text-primary'
-                    )}
-                    onClick={() => setTeam('Tim Efektif')}
-                >
-                    Tim Efektif
-                </Button>
+            <div className="flex gap-10 bg-secondary px-8 py-20 text-white">
+                <div className="-mt-5 flex flex-col">
+                    <img
+                        src="/images/teams/bu-lena.png"
+                        className="mb-2 h-96"
+                    />
+                    <h6 className="text-center text-2xl font-bold">
+                        Sopia Lena, SE, M.Si
+                    </h6>
+                    <p className="text-center text-sm">
+                        Kepala Bagian Keuangan dan Akuntansi
+                    </p>
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold">Tentang Kami</h2>
+                    <p className="mb-8 text-5xl font-extrabold tracking-wide">
+                        Keuangan RSJD AHM
+                    </p>
+                    <p className="max-w-xl">
+                        Bagian Keuangan RSJD Atma Husada Mahakam adalah bagian
+                        yang bertugas mengelola keuangan di rumah sakit. Bagian
+                        ini memiliki beberapa tim yang bertugas mengelola
+                        keuangan di rumah sakit.
+                    </p>
+                </div>
             </div>
-            <div className="px-28 py-10">
+            <div className="px-8 py-20">
+                <h2 className="text-3xl font-bold">Perkenalkan</h2>
+                <h2 className="mb-8 text-5xl font-extrabold tracking-wide">
+                    Tim Kami
+                </h2>
+                <div className="mx-auto flex gap-3 text-sm">
+                    <Button
+                        variant="outline"
+                        className={cn(
+                            team === 'Semua' &&
+                                'border border-primary text-primary'
+                        )}
+                        onClick={() => setTeam('Semua')}
+                    >
+                        Semua
+                    </Button>
+                    <Button
+                        variant="outline"
+                        className={cn(
+                            team === 'Perbendaharaan' &&
+                                'border border-primary text-primary'
+                        )}
+                        onClick={() => setTeam('Perbendaharaan')}
+                    >
+                        Perbendaharaan
+                    </Button>
+                    <Button
+                        variant="outline"
+                        className={cn(
+                            team === 'Akuntansi' &&
+                                'border border-primary text-primary'
+                        )}
+                        onClick={() => setTeam('Akuntansi')}
+                    >
+                        Akuntansi
+                    </Button>
+                    <Button
+                        variant="outline"
+                        className={cn(
+                            team === 'Verifikasi' &&
+                                'border border-primary text-primary'
+                        )}
+                        onClick={() => setTeam('Verifikasi')}
+                    >
+                        JPK Center
+                    </Button>
+                    <Button
+                        variant="outline"
+                        className={cn(
+                            team === 'Tim Efektif' &&
+                                'border border-primary text-primary'
+                        )}
+                        onClick={() => setTeam('Tim Efektif')}
+                    >
+                        Tim Efektif
+                    </Button>
+                </div>
                 <div
                     className={cn(
+                        'pt-10',
                         team === 'Semua'
-                            ? 'grid grid-cols-4 gap-3'
-                            : 'grid grid-cols-6 gap-5'
+                            ? 'grid grid-cols-4 gap-5'
+                            : 'grid grid-cols-5 gap-10'
                     )}
                 >
                     {list
@@ -215,8 +242,8 @@ export default function Home() {
                                     className={cn(
                                         'rounded-full',
                                         team === 'Semua'
-                                            ? 'w-h-20 h-20'
-                                            : 'mx-auto h-44 w-44'
+                                            ? 'h-20 w-20'
+                                            : 'mx-auto'
                                     )}
                                 />
                                 <div>
@@ -244,7 +271,34 @@ export default function Home() {
                         ))}
                 </div>
             </div>
-            <div className="bg-slate-500 p-8"></div>
+            <footer className="flex justify-between bg-primary p-8 text-sm text-white">
+                <p>
+                    <img
+                        src="/images/logo-sipeka-light.svg"
+                        className="mr-3 inline-flex h-6"
+                    />
+                    &copy; {new Date().getFullYear()} RSJD Atma Husada Mahakam.
+                    Hak Cipta dilindungi oleh undang-undang
+                </p>
+                <p>
+                    Build with{' '}
+                    <FaHeart className="inline-block h-5 w-5 text-red-500" />,
+                    <img
+                        src="/images/icons/react.png"
+                        className="inline-block h-5"
+                    />
+                    ,{' '}
+                    <img
+                        src="/images/icons/tailwind.png"
+                        className="inline-block h-5"
+                    />{' '}
+                    , and{' '}
+                    <img
+                        src="/images/icons/hono.png"
+                        className="inline-block h-5"
+                    />
+                </p>
+            </footer>
         </div>
     )
 }
---
+++ src/app/routes/panduan/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/web/components/ui/button'
+import { FiDownload } from 'react-icons/fi'
+
+export default function Page() {
+    return (
+        <div className="flex w-full flex-col px-8 py-5">
+            <h2 className="text-3xl font-extrabold">
+                Panduan Penggunaan Aplikasi
+            </h2>
+            <div className="py-5">
+                <p>
+                    Aplikasi ini memiliki beberapa fitur yang dapat digunakan
+                    oleh pengguna.
+                </p>
+            </div>
+            <Button asChild size="lg" className="mr-auto">
+                <a href="https://wa.me/6285172277277">
+                    <FiDownload className="mr-2 h-5 w-5" />
+                    Download EBook Panduan
+                </a>
+            </Button>
+        </div>
+    )
+}
