8b0321db59fc2b4c1d9f8ae737ac30c076e58a8a
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-06-07 14:57:38 +0800

    refactor: fix manifest

--- vite.config.ts
+++ vite.config.ts
@@ -13,18 +13,6 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'src'),
         },
     },
-    plugins: [
-        react(),
-
-        VitePWA({
-            registerType: 'prompt',
-            devOptions: {
-                enabled: false,
-            },
-            outDir: path.resolve(__dirname, 'dist'),
-            srcDir: path.resolve(__dirname, 'src/web'),
-        }),
-    ],
     server: {
         proxy: {
             '/api': `http://localhost:${Number(process.env.PORT ?? 3000)}`,
@@ -36,4 +24,64 @@ export default defineConfig({
         emptyOutDir: true,
     },
     publicDir: path.resolve(__dirname, 'public'),
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'prompt',
+            devOptions: {
+                enabled: false,
+            },
+            outDir: path.resolve(__dirname, 'dist'),
+            srcDir: path.resolve(__dirname, 'src/web'),
+            manifest: {
+                name: 'Aplikasi SIPEKA',
+                short_name: 'SIPEKA',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                description: 'Sistem Informasi Pengelolaan Keuangan BLUD',
+                scope: '/',
+                orientation: 'portrait',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/images/icons/icon-72x72.png',
+                        sizes: '72x72',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: '/images/icons/icon-96x96.png',
+                        sizes: '96x96',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: '/images/icons/icon-128x128.png',
+                        sizes: '128x128',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: '/images/icons/icon-152x152.png',
+                        sizes: '152x152',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: '/images/icons/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: '/images/icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
+    ],
 })
