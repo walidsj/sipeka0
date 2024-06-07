11e84d13e8dd48093361ea5f917d890a1ddfe1b7
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-06-07 10:43:15 +0800

    refactor: rab

--- src/app/schema/rab.ts
+++ src/app/schema/rab.ts
@@ -3,5 +3,5 @@ import { z } from 'zod'
 export const rabSchema = z.object({
     kodeRekening: z.string().min(1),
     uraian: z.string().min(1),
-    spesifikasi: z.string().min(1),
+    spesifikasi: z.string().nullish(),
 })
