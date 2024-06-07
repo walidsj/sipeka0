93b52e1afd3b2f44a6535e327bea2a16b475a25b
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-06-07 14:43:41 +0800

    refactor: remove fontsource change to embed in html font google

--- src/web/providers.tsx
+++ src/web/providers.tsx
@@ -1,5 +1,4 @@
 import '@/web/styles/index.css'
-import '@fontsource-variable/plus-jakarta-sans'
 import Router from './router'
 import { Toaster } from 'react-hot-toast'
 import { TRPCReactProvider } from '@/web/trpc/react'
