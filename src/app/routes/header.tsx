b1bee8b114f1848e50b78e53932ac75ae1179d5a
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-07-04 10:49:22 +0800

    fix: bug in indicator

--- src/app/routes/header.tsx
+++ src/app/routes/header.tsx
@@ -43,7 +43,7 @@ export function Header() {
                 ) {
                     socket.emit('online', {
                         user: auth.user.nama,
-                        isActive: false,
+                        isActive: true,
                     })
                 }
             } else {
