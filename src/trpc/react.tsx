224df8493e03964003e0dc8848bfdd8fc7babb83
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-07-14 01:14:44 +0800

    refactor: update with unstable_httpBatchStreamLink

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { loggerLink, httpBatchLink } from '@trpc/client'
+import { loggerLink, unstable_httpBatchStreamLink } from '@trpc/client'
 import { createTRPCReact } from '@trpc/react-query'
 import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
 import SuperJSON from 'superjson'
@@ -34,7 +34,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
                     import.meta.env.DEV ||
                     (op.direction === 'down' && op.result instanceof Error),
             }),
-            httpBatchLink({
+            unstable_httpBatchStreamLink({
                 transformer: SuperJSON,
                 url: '/api/trpc',
                 headers: () => {
