43f52c1ce03c1eadae3b1a7fc3938e47a64f2662
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-07-17 17:16:57 +0800

    refactor: update lpj GU

--- src/server/api/routers/lpj_belanja.ts
+++ src/server/api/routers/lpj_belanja.ts
@@ -124,6 +124,10 @@ export const lpjBelanjaRouter = createTRPCRouter({
                     desc(belanja.noDokumen),
                     desc(belanja.createdAt),
                 ],
+                with: {
+                    rab: true,
+                    potonganBelanja: true,
+                },
             })
         }),
 
