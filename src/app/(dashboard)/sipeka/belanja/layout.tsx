0ddf255af988db1c6cf9ca0c201247f9aaf0c9c9
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-07-18 08:45:05 +0800

    refactor: update layout

--- src/app/(dashboard)/sipeka/belanja/layout.tsx
+++ src/app/(dashboard)/sipeka/belanja/layout.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { NavLink, Outlet } from 'react-router-dom'
 import { HiOutlineBookmarkAlt, HiOutlineClipboardList } from 'react-icons/hi'
+import { cn } from '@/lib/utils'
 
 export default function Layout() {
     return (
@@ -8,38 +9,116 @@ export default function Layout() {
             <nav className="mx-auto flex w-full overflow-x-auto">
                 <Button variant="ghost" asChild>
                     <NavLink to="perekaman">
-                        <HiOutlineClipboardList className="mr-1 h-5 w-5" />
-                        <span>Rekam</span>
+                        {({ isActive }) => (
+                            <>
+                                <HiOutlineClipboardList
+                                    className={cn(
+                                        'mr-1 h-5 w-5',
+                                        isActive && 'text-primary'
+                                    )}
+                                />
+                                <span
+                                    className={cn(isActive && 'text-primary')}
+                                >
+                                    Rekam
+                                </span>
+                            </>
+                        )}
                     </NavLink>
                 </Button>
                 <Button variant="ghost" asChild>
                     <NavLink to="lpj-belanja">
-                        <HiOutlineBookmarkAlt className="mr-1 h-5 w-5" />
-                        <span>LPJ Belanja</span>
+                        {({ isActive }) => (
+                            <>
+                                <HiOutlineBookmarkAlt
+                                    className={cn(
+                                        'mr-1 h-5 w-5',
+                                        isActive && 'text-primary'
+                                    )}
+                                />
+                                <span
+                                    className={cn(isActive && 'text-primary')}
+                                >
+                                    LPJ Belanja
+                                </span>
+                            </>
+                        )}
                     </NavLink>
                 </Button>
                 <Button variant="ghost" asChild>
                     <NavLink to="spp">
-                        <HiOutlineBookmarkAlt className="mr-1 h-5 w-5" />
-                        <span>SPP</span>
+                        {({ isActive }) => (
+                            <>
+                                <HiOutlineBookmarkAlt
+                                    className={cn(
+                                        'mr-1 h-5 w-5',
+                                        isActive && 'text-primary'
+                                    )}
+                                />
+                                <span
+                                    className={cn(isActive && 'text-primary')}
+                                >
+                                    SPP
+                                </span>
+                            </>
+                        )}
                     </NavLink>
                 </Button>
                 <Button variant="ghost" asChild>
                     <NavLink to="spm">
-                        <HiOutlineBookmarkAlt className="mr-1 h-5 w-5" />
-                        <span>SPM</span>
+                        {({ isActive }) => (
+                            <>
+                                <HiOutlineBookmarkAlt
+                                    className={cn(
+                                        'mr-1 h-5 w-5',
+                                        isActive && 'text-primary'
+                                    )}
+                                />
+                                <span
+                                    className={cn(isActive && 'text-primary')}
+                                >
+                                    SPM
+                                </span>
+                            </>
+                        )}
                     </NavLink>
                 </Button>
                 <Button variant="ghost" asChild>
                     <NavLink to="sp2d">
-                        <HiOutlineBookmarkAlt className="mr-1 h-5 w-5" />
-                        <span>SP2D</span>
+                        {({ isActive }) => (
+                            <>
+                                <HiOutlineBookmarkAlt
+                                    className={cn(
+                                        'mr-1 h-5 w-5',
+                                        isActive && 'text-primary'
+                                    )}
+                                />
+                                <span
+                                    className={cn(isActive && 'text-primary')}
+                                >
+                                    SP2D
+                                </span>
+                            </>
+                        )}
                     </NavLink>
                 </Button>
                 <Button variant="ghost" asChild>
                     <NavLink to="buku/kas-umum">
-                        <HiOutlineBookmarkAlt className="mr-1 h-5 w-5" />
-                        <span>Buku Bendahara</span>
+                        {({ isActive }) => (
+                            <>
+                                <HiOutlineBookmarkAlt
+                                    className={cn(
+                                        'mr-1 h-5 w-5',
+                                        isActive && 'text-primary'
+                                    )}
+                                />
+                                <span
+                                    className={cn(isActive && 'text-primary')}
+                                >
+                                    Buku Bendahara
+                                </span>
+                            </>
+                        )}
                     </NavLink>
                 </Button>
             </nav>
