aad0385cabee37c0132c1cee25f5bd334df56bfa
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-07-21 05:24:02 +0800

    refactor": update

--- src/app/(dashboard)/myatma/page.tsx
+++ src/app/(dashboard)/myatma/page.tsx
@@ -1,14 +1,147 @@
-import { CardDescription, CardTitle } from '@/components/ui/card'
-import { FaHeart } from 'react-icons/fa6'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import { useAuth } from '@/lib/auth'
+import { ChartContainer, type ChartConfig } from '@/components/ui/chart'
+import { Bar, BarChart, Legend, XAxis, YAxis } from 'recharts'
+
+const chartConfig = {
+    gaji: {
+        label: 'Gaji',
+        color: '#22c55e',
+    },
+    tunjangan: {
+        label: 'Tunjangan',
+        color: '#60a5fa',
+    },
+    jasaPelayanan: {
+        label: 'Jasa Pelayanan',
+        color: '#eab308',
+    },
+} satisfies ChartConfig
 
 export default function Page() {
+    const auth = useAuth()
+
+    const chartData = [
+        { month: 'January', gaji: 186, tunjangan: 80, jasaPelayanan: 50 },
+        { month: 'February', gaji: 305, tunjangan: 200, jasaPelayanan: 100 },
+        { month: 'March', gaji: 237, tunjangan: 120, jasaPelayanan: 80 },
+        { month: 'April', gaji: 73, tunjangan: 190, jasaPelayanan: 70 },
+        { month: 'May', gaji: 209, tunjangan: 130, jasaPelayanan: 60 },
+        { month: 'June', gaji: 214, tunjangan: 140, jasaPelayanan: 50 },
+    ]
+
     return (
-        <div className="pt-10 text-center">
-            <CardTitle className="text-7xl">MyAtma</CardTitle>
-            <CardDescription className="text-3xl">
-                Coming Soon{' '}
-                <FaHeart className="inline-block h-8 w-8 text-red-500" />
-            </CardDescription>
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Data Pegawai</CardTitle>
+                    <CardDescription>Data diri pegawai</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    {auth.user?.pegawai &&
+                        ((
+                            <Table className="w-full">
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell className="font-bold">
+                                            NIP
+                                        </TableCell>
+                                        <TableCell>
+                                            {auth.user.pegawai.nip}
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-bold">
+                                            NIK
+                                        </TableCell>
+                                        <TableCell>
+                                            {auth.user.pegawai.nik}
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-bold">
+                                            NPWP
+                                        </TableCell>
+                                        <TableCell>
+                                            {auth.user.pegawai.npwp}
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-bold">
+                                            No. Telepon
+                                        </TableCell>
+                                        <TableCell>
+                                            {auth.user.pegawai.noTelp}
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-bold">
+                                            Jenis Kelamin
+                                        </TableCell>
+                                        <TableCell>
+                                            {auth.user.pegawai.jenisKelamin}
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-bold">
+                                            No. Rekening
+                                        </TableCell>
+                                        <TableCell>
+                                            {auth.user.pegawai.noRekening}
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-bold">
+                                            Nama Rekening
+                                        </TableCell>
+                                        <TableCell>
+                                            {auth.user.pegawai.namaRekening}
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        ) || <CardTitle>Profil belum terkoneksi</CardTitle>)}
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Grafik Penghasilan</CardTitle>
+                    <CardDescription>
+                        Data penghasilan bersih per bulan
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <ChartContainer config={chartConfig} className="w-full">
+                        <BarChart accessibilityLayer data={chartData}>
+                            <YAxis />
+                            <XAxis dataKey="month" />
+                            <Bar
+                                dataKey="gaji"
+                                fill="var(--color-gaji)"
+                                radius={10}
+                            />
+                            <Bar
+                                dataKey="tunjangan"
+                                fill="var(--color-tunjangan)"
+                                radius={10}
+                            />
+                            <Bar
+                                dataKey="jasaPelayanan"
+                                fill="var(--color-jasaPelayanan)"
+                                radius={10}
+                            />
+                            <Legend />
+                        </BarChart>
+                    </ChartContainer>
+                </CardContent>
+            </Card>
         </div>
     )
 }
