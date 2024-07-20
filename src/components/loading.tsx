cd058e410732f5f81a85a23cac6019225ef458da
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-07-21 01:05:43 +0800

    refactor: update

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,11 +1,18 @@
+import { Skeleton } from './ui/skeleton'
+
 export default function Loading() {
     return (
         <div className="flex w-full items-center justify-center">
-            <img
+            {/* <img
                 src="/images/icons/loading.svg"
                 alt="loading"
                 className="h-14 w-14"
-            />
+            /> */}
+            <div className="flex gap-2">
+                <Skeleton className="h-3 w-3 rounded-full" />
+                <Skeleton className="h-3 w-3 rounded-full" />
+                <Skeleton className="h-3 w-3 rounded-full" />
+            </div>
         </div>
     )
 }
