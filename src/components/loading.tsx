1f95534c17bc31e403cf09e4a4fffd52a1280a12
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-07-21 17:54:23 +0800

    refactor: loading component

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from './ui/skeleton'
 export default function Loading() {
     return (
         <div className="flex w-full items-center justify-center">
-            <div className="flex gap-2">
+            <div className="flex gap-2 py-10">
                 <Skeleton className="h-3 w-3 rounded-full" />
                 <Skeleton className="h-3 w-3 rounded-full" />
                 <Skeleton className="h-3 w-3 rounded-full" />
