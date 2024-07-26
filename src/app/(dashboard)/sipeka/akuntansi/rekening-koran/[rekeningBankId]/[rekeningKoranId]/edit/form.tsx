b4477727bea5f726e754de008fe1f9a60455f3cc
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-07-26 09:51:23 +0800

    refactor: edit form

--- src/app/(dashboard)/sipeka/akuntansi/rekening-koran/[rekeningBankId]/[rekeningKoranId]/edit/form.tsx
+++ src/app/(dashboard)/sipeka/akuntansi/rekening-koran/[rekeningBankId]/[rekeningKoranId]/edit/form.tsx
@@ -40,6 +40,7 @@ export default function EditForm({
             noReferensi: data.noReferensi || undefined,
             debet: Number(data.debet) || undefined,
             kredit: Number(data.kredit) || undefined,
+            keteranganTambahan: data.keteranganTambahan || undefined,
         },
     })
 
