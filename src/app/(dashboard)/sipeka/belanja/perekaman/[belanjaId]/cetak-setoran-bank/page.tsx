382be86777933efcf68b4ba60952ac179d54dcea
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-07-29 12:41:35 +0800

    fix: padding left facheck

--- src/app/(dashboard)/sipeka/belanja/perekaman/[belanjaId]/cetak-setoran-bank/page.tsx
+++ src/app/(dashboard)/sipeka/belanja/perekaman/[belanjaId]/cetak-setoran-bank/page.tsx
@@ -284,7 +284,7 @@ export default function Page() {
                                             <table className="text-black-500 w-[100%] border-separate border-spacing-[0.25mm]">
                                                 <tbody>
                                                     <tr>
-                                                        <td className="h-[4mm] border border-transparent py-0 pl-[7.85mm] align-top font-arial text-[8pt] leading-[9pt]">
+                                                        <td className="h-[4mm] border border-transparent py-0 pl-[1cm] align-top font-arial text-[8pt] leading-[9pt]">
                                                             <FaCheck />
                                                         </td>
                                                     </tr>
