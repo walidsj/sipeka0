35980b33bb75ef16db37f36dd412c669378879a0
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2025-04-26 10:19:04 +0800

    refactor: remove socket

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,6 @@ import { api } from '@/trpc/react'
 import { getQueryKey } from '@trpc/react-query'
 import { useQueryClient } from '@tanstack/react-query'
 import toast from 'react-hot-toast'
-import { socket } from './socket'
 
 export function useAuth() {
     const [cookies, setCookie, removeCookie] = useCookies(['token'])
@@ -34,7 +33,6 @@ export function useAuth() {
     function logout() {
         const userKey = getQueryKey(api.user.getProfile, undefined)
         queryClient.removeQueries({ queryKey: userKey })
-        socket.emit('logout')
         removeCookie('token')
     }
 
--- src/lib/socket.ts
+++
@@ -1,6 +0,0 @@
-import { io } from 'socket.io-client'
-
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.PROD ? '' : 'http://localhost:8989'
-
-export const socket = io(URL)
